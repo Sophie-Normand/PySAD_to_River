6e2ef3a69930069ede3043a09be8c7c85f498672
Sophie-Normand <sophie.normand@telecom-sudparis.eu>
2022-12-26 23:15:48 +0100

    streamhash_projector

--- transform/projection/streamhash_projector.py
+++ transform/projection/streamhash_projector.py
@@ -2,6 +2,10 @@
 import numpy as np
 #from pysad.core.base_transformer import BaseTransformer
 from river.utils import dict2numpy
+from river.utils import numpy2dict
+import math
+from river.utils.math import dot
+from river.utils.math import dotvecmat
 
 from base.transformer import BaseTransformer
 
@@ -20,7 +24,7 @@ class StreamhashProjector(BaseTransformer):
     def __init__(self, num_components, density=1 / 3.0):
         super().__init__(num_components)
         self.keys = np.arange(0, num_components, 1)
-        self.constant = np.sqrt(1. / density) / np.sqrt(num_components)
+        self.constant = math.sqrt(1. / density) / math.sqrt(num_components)
         self.density = density
         self.n_components = num_components
 
@@ -50,18 +54,35 @@ class StreamhashProjector(BaseTransformer):
             Projected feature vector.
         """
         
-        X = dict2numpy(X)
-        X = X.reshape(1, -1)
+    #    print(len(X))
+    #    X = dict2numpy(X)
+    #    X = X.reshape(1, -1)
+    #    print(X)
 
-        ndim = X.shape[1]
+    #    ndim = X.shape[1]
+
+        ndim = len(X)
 
         feature_names = [str(i) for i in range(ndim)]
 
         R = np.array([[self._hash_string(k, f)
                        for f in feature_names]
                       for k in self.keys])
+        
+    #    print(R)
+
+    #    R = numpy2dict(R)
+
+        X = dict2numpy(X)
+        X = X.reshape(1, -1)
 
         Y = np.dot(X, R.T).squeeze()
+    #    Y = dot(X,R)
+    #    Y = dotvecmat(X, R)
+
+    #    print(Y)
+
+
 
         return Y
 
